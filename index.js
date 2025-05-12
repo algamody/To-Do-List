2a95090f8a0a6b4e1a9ccb568cd497a5c1980445
algamody <212273@cctt.edu.ly>
2025-05-12 14:52:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    $('#loginForm').on('submit', function (e) {
+        e.preventDefault(); // منع الإرسال التلقائي
+
+        let email = $('#floatingInput').val().trim();
+        let password = $('#floatingPassword').val();
+
+        // تحقق من البريد الإلكتروني باستخدام RegEx
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailPattern.test(email)) {
+            alert('يرجى إدخال بريد إلكتروني صالح.');
+            return;
+        }
+
+        if (password.length < 6) {
+            alert('كلمة المرور يجب أن تكون 6 أحرف على الأقل.');
+            return;
+        }
+
+        // إذا التحقق ناجح
+
+        if (email === 'admin@project.ly' && password === '12345678') {
+            alert('تم تسجيل الدخول بنجاح');
+            window.location.href = 'main.html';
+            return;
+        }
+        else {
+            alert('البريد الإلكتروني أو كلمة المرور غير صحيحة');
+            return;
+        }
+
+        // يمكنك إرسال النموذج باستخدام AJAX أو this.submit()
+    });
+
+
+    $('#log-out').on('click', function (e) {
+        e.preventDefault(); // منع الإرسال التلقائي
+        alert('تم تسجيل الخروج بنجاح');
+        window.location.href = 'index.html';
+    });
+});
