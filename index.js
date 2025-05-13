9d95c9a10095463be73c431d36cb96006c288613
algamody <212273@cctt.edu.ly>
2025-05-13 15:33:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
         }
 
         if (email === 'admin@project.ly' && password === '12345678') {
-            alert('تم تسجيل الدخول بنجاح');
+            sessionStorage.setItem("isLoggedIn", "true");
             window.location.href = 'main.html';
             return;
         } else {
@@ -52,8 +52,17 @@ $(document).ready(function () {
                         <span class="task-text">${taskText}</span>
                     </div>
                     <div>
-                        <button class="btn btn-success btn-sm me-2 complete-btn">✓</button>
-                        <button class="btn btn-danger btn-sm delete-btn">✗</button>
+                        <button class="btn btn-success btn-sm me-1 complete-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+                        <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                        <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                        </svg></button>
+
+                        <button class="btn btn-danger btn-sm m-1 delete-btn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                        </svg></button>
+                        
+                        <button class="btn btn-secondary btn-sm m-1 edit-btn">✎</button>
                     </div>
                 </li>
             `;
@@ -77,9 +86,21 @@ $(document).ready(function () {
         saveTasks();
     });
 
+    // Task editing
+    $("#taskList").on("click", ".edit-btn", function () {
+        const taskText = $(this).closest("li").find(".task-text").text();
+        const newTaskText = prompt("تعديل المهمة:", taskText);
+        // Check if the user entered a new task text
+        if (newTaskText !== null) {
+            $(this).closest("li").find(".task-text").text(newTaskText);
+            saveTasks();
+        }
+    });
+
     // Logout button
     $('#logout').on('click', function (e) {
         e.preventDefault();
+        sessionStorage.setItem("isLoggedIn", "false");
         window.location.href = 'index.html';
     });
 
